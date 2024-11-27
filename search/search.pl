56595b5e4503de0e87218169c649d78373cee4d0
rnujella <rnujella@binghamton.edu>
2024-11-27 16:38:07 -0500

    Update search.pl

--- search/search.pl
+++ search/search.pl
@@ -1,18 +1,24 @@
 search(Actions) :-
     initial(Start),
     treasure(Target),
+    write('Starting from room: '), write(Start), nl,
+    write('Looking for treasure at room: '), write(Target), nl,
     bfs([[state(Start, [], [])]], Target, Actions).
 
 bfs([[state(Room, Keys, Path)|_]|_], Target, Actions) :-
     Room = Target,
+    write('Found treasure at room: '), write(Room), nl,
     reverse(Path, Actions).
 
 bfs([[state(Room, Keys, Path)|Rest]|Queue], Target, Actions) :-
+    write('Exploring room: '), write(Room), nl,
+    write('Collected keys: '), write(Keys), nl,
     findall(
         NextState,
         move_to_next(Room, Keys, Path, NextState),
         NextStates
     ),
+    write('Next possible states: '), write(NextStates), nl,
     append(Rest, NextStates, UpdatedQueue),
     bfs(UpdatedQueue, Target, Actions).
 
@@ -22,8 +28,10 @@ move_to_next(CurrentRoom, Keys, Path, state(NextRoom, Keys, [move(CurrentRoom, N
         member(LockColor, Keys)
     ;   \+ locked_door(CurrentRoom, NextRoom, _)
     ),
+    write('Moving from room '), write(CurrentRoom), write(' to room '), write(NextRoom), nl,
     \+ member(move(CurrentRoom, NextRoom), Path).
 
 move_to_next(CurrentRoom, Keys, Path, state(CurrentRoom, [NewKey|Keys], [take(CurrentRoom, NewKey)|Path])) :-
     key(CurrentRoom, NewKey),
-    \+ member(NewKey, Keys).
+    \+ member(NewKey, Keys),
+    write('Picked up key: '), write(NewKey), nl.
